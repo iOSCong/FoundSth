47085e9b70cc80de580f6e6d1aebc58f2180c58b
iOS <1876454300@qq.com>
2018-12-24 17:00:52 +0800

    mjg 列表图片

--- FoundSth/myClass/FoundListViewController.m
+++ FoundSth/myClass/FoundListViewController.m
@@ -8,10 +8,14 @@
 
 #import "FoundListViewController.h"
 #import "FoundListTableViewCell.h"
+#import "YBImageBrowser.h"
 
-@interface FoundListViewController () <UITableViewDelegate,UITableViewDataSource>
+@interface FoundListViewController () <UITableViewDelegate,UITableViewDataSource,YBImageBrowserDelegate,YBImageBrowserDataSource>
 
 @property (nonatomic,strong)NSArray *dataArr;
+@property (nonatomic,assign)NSInteger indexp;
+@property (nonatomic,strong)UIImage *tempImg;
+@property (nonatomic,strong)NSMutableArray *imgArr;
 
 @end
 
@@ -31,6 +35,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    self.imgArr = [NSMutableArray array];
     
     self.tableView.hidden = NO;
     self.tableView.delegate = self;
@@ -77,7 +83,7 @@
     AVUser *owner = self.dataArr[indexPath.row][@"owner"];
     AVFile *userAvatar =[owner objectForKey:@"avatar"];
     if (userAvatar) {
-        NSLog(@"userAvatar==%@",userAvatar.url);
+//        NSLog(@"userAvatar==%@",userAvatar.url);
         [cell.headImgView sd_setImageWithURL:[NSURL URLWithString:userAvatar.url] placeholderImage:[UIImage imageNamed:@"placehoald"]];
     }
     cell.nameLabel.text = self.dataArr[indexPath.row][@"title"];
@@ -88,9 +94,23 @@
     cell.timeLabel.text = [dateFormatter stringFromDate:createdAt];
     
     AVFile *imageFile = self.dataArr[indexPath.row][@"image"];
+    if (imageFile) {
 //    NSLog(@"imageFile==%@",imageFile.url);
-    [cell.contentImgView sd_setImageWithURL:[NSURL URLWithString:imageFile.url] placeholderImage:[UIImage imageNamed:@"placehoald"]];
-
+        [cell.contentImgView sd_setImageWithURL:[NSURL URLWithString:imageFile.url] placeholderImage:[UIImage imageNamed:@"placehoald"] completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+            self.tempImg = image;
+            NSMutableDictionary *dic = [NSMutableDictionary dictionary];
+            [dic setValue:[NSNumber numberWithInteger:indexPath.row] forKey:@"index"];
+            [dic setValue:image forKey:@"image"];
+            [self.imgArr addObject:dic];
+            NSLog(@"%ld===%@",indexPath.row, self.imgArr[indexPath.row]);
+        }];
+        cell.contentImgView.userInteractionEnabled = YES;
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapHeadImgViewHandle)];
+        [cell.contentImgView addGestureRecognizer:tap];
+        self.tempImg = cell.contentImgView.image;
+        self.indexp = indexPath.row;
+    }
+    
     cell.detailLabel.text = self.dataArr[indexPath.row][@"detail"];
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
     return cell;
@@ -102,7 +122,47 @@
     
 }
 
+#pragma mark 配置数据源
+- (void)tapHeadImgViewHandle
+{
+    YBImageBrowserModel *model = [YBImageBrowserModel new];
+    AVFile *imageFile = self.dataArr[self.indexp][@"image"];
+    if (imageFile) {
+        NSLog(@"url==%@",imageFile.url);
+        [MHProgressHUD showProgress:@"加载中..." inView:self.view];
+        NSData * data = [NSData dataWithContentsOfURL:[NSURL URLWithString:imageFile.url]];
+        [model setImageWithImageName:[UIImage imageWithData:data]];
+        [MHProgressHUD hide];
+        YBImageBrowser *browser = [YBImageBrowser new];
+        browser.dataArray = @[model];
+        browser.currentIndex = 0;
+        [browser show];
+    }
+}
 
+
+#pragma mark 使用数组配置数据源
+//- (void)tapHeadImgViewHandle
+//{
+//    //创建图片浏览器
+//    YBImageBrowser *browser = [YBImageBrowser new];
+//    browser.dataSource = self;
+//    browser.currentIndex = 0;
+//    [browser show];
+//}
+////YBImageBrowserDataSource 代理实现赋值数据
+//- (NSInteger)numberInYBImageBrowser:(YBImageBrowser *)imageBrowser {
+//    return 1;
+//}
+//- (YBImageBrowserModel *)yBImageBrowser:(YBImageBrowser *)imageBrowser modelForCellAtIndex:(NSInteger)index {
+//    YBImageBrowserModel *model = [YBImageBrowserModel new];
+//    AVFile *imageFile = self.dataArr[self.indexp][@"image"];
+//    model.url = [NSURL URLWithString:[imageFile.url stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+//    return model;
+//}
+//- (UIImageView * _Nullable)imageViewOfTouchForImageBrowser:(nonnull YBImageBrowser *)imageBrowser {
+//    return nil;
+//}
 
 
 
