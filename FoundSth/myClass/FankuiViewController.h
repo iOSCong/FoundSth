03d8dda80c2a748095ebcc887fb1ca90f11fa18e
qbsqbq <610445366@qq.com>
2018-12-27 18:36:15 +0800

    测试

--- FoundSth/myClass/FankuiViewController.h
+++ FoundSth/myClass/FankuiViewController.h
@@ -6,8 +6,7 @@
 //  Copyright © 2018年 MCEJ. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
-@interface FankuiViewController : UIViewController
+#import "BaseViewController.h"
+@interface FankuiViewController : BaseViewController
 
 @end
