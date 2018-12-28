bd878abb54e652f67c59a904d1b0adc9afb5402c
iOS <1876454300@qq.com>
2018-12-28 16:01:32 +0800

    备注

--- FoundSth/BaseClass/AppDelegate.m
+++ FoundSth/BaseClass/AppDelegate.m
@@ -30,7 +30,7 @@
 @end
 
 @implementation AppDelegate
-
+//master
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
